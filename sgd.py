7e12298f2ce34737ef835c8528d1b399c5692adb
Ian Horsman <ianphorsman@gmail.com>
2017-12-27 23:41:14 -0500

    SGD finished

--- sgd.py
+++ sgd.py
@@ -10,10 +10,10 @@ import pdb
 
 class SGD(object):
 
-    def __init__(self, X, y, filename='sgd', inspect_rate=50, iterations=1000, learning_rate=0.000025, nodes=(2,3,1), batch_size=50):
+    def __init__(self, X, y, filename='sgd', inspect_rate=50, iterations=1000, learning_rate=0.000025, nodes=(2,3,1), batch_size=50, activation_function='sigmoid'):
         # store input and expected output data
         self.X = X
-        self.y = y
+        self.y = np.atleast_2d(y)
 
         # saved network name
         self.filename = filename
@@ -27,60 +27,120 @@ class SGD(object):
         # learning hyperparameters
         self.learning_rate = learning_rate
 
+        # activation function
+        self.activation_function = activation_function
+
         # network layers
         self.nodes = nodes
-
         # store costs and accuracy over iterations
         self.costs = []
         self.accuracies = []
 
-        # initialize placeholder nodes
-        self.initialize_nodes()
+        # initialize placeholder layers
+        self.layers = self.initialize_layers()
 
-        # initialize placeholder weights
-        self.initialize_weights()
+        # initialize placeholder weights and deltas
+        self.weights = self.initialize_weights()
+        #self.deltas = self.initialize_deltas()
 
-
-    def initialize_nodes(self):
-        for i in range(len(self.nodes)):
-            setattr(self, "layer_{}".format(i), np.atleast_2d(np.ones(self.nodes[i])))
+    def initialize_layers(self):
+        layers = []
+        return layers
 
     def initialize_weights(self):
-        for i in range(len(self.nodes)-1):
-            pass
+        weights = []
+        for i in range(1, len(self.nodes)):
+            weights.append(np.random.randn(self.nodes[i - 1] + 1, self.nodes[i]))
+        return weights
 
     def train(self, X):
         for i in range(self.iterations):
-            cost = 0
+            hypothesis = self.feedforward(X)
+            self.backpropagate(hypothesis)
+            cost = np.average(0.5 * ((self.y - hypothesis) ** 2))
             if i % self.inspect_rate == 0:
                 self.costs.append(cost)
-                accuracy = self.test_accuracy()
+                accuracy = self.test_accuracy(X, self.y)
                 self.accuracies.append(accuracy)
                 print(self.inspect_performance(i, cost, accuracy))
 
-    def test_accuracy(self):
-        pass
+    def test_accuracy(self, X_test, y_test):
+        hypothesis = self.feedforward(X_test)
+        #pdb.set_trace()
+        return np.sum(np.round(hypothesis) == y_test.T) / X_test.shape[0] * 100
+
 
     def next_batch(self):
         pass
 
+    def add_bias(self, x):
+        return np.hstack((np.ones((x.shape[0], 1)), x))
+
     def feedforward(self, X):
-        pass
+        num_weight_matrices = len(self.weights)
+        layer = self.add_bias(X)
+        self.layers.append(layer)
+        for i in range(num_weight_matrices):
+            #pdb.set_trace()
+            if i == num_weight_matrices - 1:
+                w = self.weights[i]
+                layer = self.activate(np.dot(layer, w))
+            else:
+                w = self.weights[i]
+                layer = self.add_bias(self.activate(np.dot(layer, w)))
+            self.layers.append(layer)
+        return layer
 
-    def backpropagate(self, y):
-        pass
+    def backpropagate(self, hypothesis):
+        num_layer_matrices = len(self.layers)
+        deltas = [self.y.T - hypothesis]
+        for layer, weights in zip(reversed(self.layers[:-1]), reversed(self.weights)):
+            prev_delta = deltas[-1]
+            #pdb.set_trace()
+            if prev_delta.shape[1] - 1 != weights.shape[1]:
+                #print("not clipped")
+                delta = prev_delta.dot(weights.T) * self.activate(layer, d=True)
+            else:
+                #print("clipped")
+                delta = prev_delta[:, 1:].dot(weights.T) * self.activate(layer, d=True)
+            deltas.append(delta)
+        for i in range(1, len(deltas)):
+            delta = deltas[i - 1]
+            layer = self.layers[:-1][-i]
+            #print("update")
+            #pdb.set_trace()
+            if i == 1:
+                self.weights[-i] += self.learning_rate * delta.T.dot(layer).T
+            else:
+                self.weights[-i] += self.learning_rate * delta[:, 1:].T.dot(layer).T
 
-    def sigmoid(self, X):
+        return deltas
+
+
+
+
+    def activate(self, X, d=False):
+        if self.activation_function == 'sigmoid':
+            return self.sigmoid(X, d)
+        elif self.activation_function == 'tanh':
+            return self.tanh(X, d)
+        else:
+            return self.relu(X, d)
+
+    def sigmoid(self, X, d=False):
+        if d:
+            return X * (1 - X)
         return 1 / (1 + np.exp(-X))
 
-    def d_sigmoid(self, X):
-        return X * (1 - X)
-
-    def tanh(self, X):
+    def tanh(self, X, d=False):
+        if d:
+            return 1 - np.tanh(X) ** 2
         return np.tanh(X)
 
-    def d_tanh(self, X):
-        return 1 - np.tanh(X) ** 2
+    def relu(self, X, d=False):
+        if d:
+            return np.multiply(1., (X > 0))
+        return np.maximum(X, 0, X)
 
     def save(self):
         picklerick.dump(self, open("{}.p".format(self.filename), 'wb'))
@@ -97,8 +157,14 @@ class SGD(object):
     def inspect_performance(self,iteration, cost, accuracy):
         return "Iteration: {} , Cost: {} , Accuracy: {}".format(iteration, cost, accuracy)
 
+    def inspect_architecture(self):
+        return [w.shape for w in self.weights] == [d.shape for d in self.deltas]
 
-X, y = make_moons(1000, noise=0.2, random_state=333)
+
+X, y = make_moons(1000, noise=0.15, random_state=333)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-sgd = SGD(X_train, y_train)
+sgd = SGD(X_train, y_train, iterations=2000, learning_rate=0.01)
+
+sgd.train(sgd.X)
+print(sgd.test_accuracy(X_test, np.atleast_2d(y_test)))
